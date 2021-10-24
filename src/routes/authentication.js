efd0bd32ffd0a373e44223596d0e6282212bd1be
Cristian Rivera <cristian.noviembre.17@gmail.com>
2021-10-24 17:35:21 -0600

    Sirve y Probando

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+
+router.get('/signup', (req, res) => {
+    res.render('user/checkin');
+});
+
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup'
+}));
+
+router.get('/profile', (req, res) => {
+    res.send('Profile');
+});
 
 
 module.exports = router;
